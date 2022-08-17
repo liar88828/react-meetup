a20f81f6f6e68cb9482e1b0bc3667fa4e0469a11
liar780@gmail.com <liar780@gmail.com>
2022-08-17 20:42:58 +0700

    add nav and nav css

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import { Route, Routes } from "react-router-dom";
 import AllMeetupPage from "./pages/AllMeetsup";
 import FavoritePages from "./pages/Favorites";
 import NewMeetupPage from "./pages/NewMeetup";
+import MainNavigator from "./components/layout/MainNavigator";
 function App() {
   return (
     <div>
+      <MainNavigator />
       <Routes>
         <Route path="/" element={<NewMeetupPage />} />
       </Routes>
       <Routes>
-        <Route path="/new-meetup" element={<AllMeetupPage />} />
+        <Route path="/new--meetup" element={<AllMeetupPage />} />
       </Routes>
       <Routes>
         <Route path="/favorites" element={<FavoritePages />} />
