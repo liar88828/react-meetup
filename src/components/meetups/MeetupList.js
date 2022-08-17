a6270012da696d626663f2b18e3a4e944266fffe
liar780@gmail.com <liar780@gmail.com>
2022-08-17 21:48:37 +0700

    fix meetups, add css , add function  AllMeetsup

--- src/components/meetups/MeetupList.js
+++ src/components/meetups/MeetupList.js
@@ -1,19 +1,19 @@
-
+import classes from "./MeetupList.module.css";
+import MeetupItem from "./MeetUpItem";
 function MeetupList(props) {
-  return (
-    <li>
-      <div>
-        <img src={props.image} alt={props.title} />
-      </div>
-      <div>
-        <h3>{props.title}</h3>
-            <address>{props.address}</address>
-        <p>{props.description}</p>
-      </div>
-      <div>
-        <button>To Favorite</button>
-      </div>
-    </li>
-  );
+ return (
+   <ul className={classes.list}>
+     {props.meetups.map(meetup =>  
+     <MeetupItem 
+     key={meetup.is}
+     id={meetup.id} 
+     image={meetup.image}
+     title={meetup.title}  
+     addrest={MeetupItem.addrest} 
+     description={meetup.description}
+     />
+     )}
+   </ul>
+ );
 }
 export default MeetupList;
