7b67a028c3054bec7844b6bad9ec4c4560e94bc1
liar780@gmail.com <liar780@gmail.com>
2022-08-17 22:18:36 +0700

    add fix css add (card and card.css)

--- src/components/meetups/MeetUpItem.js
+++ src/components/meetups/MeetUpItem.js
@@ -1,18 +1,25 @@
+import Card from "../ui/Card";
+
 import classes from "./MeetupItem.module.css";
 function MeetupItem(props) {
   return (
     <li className={classes.item}>
-      <div>
-        <img src={props.image} alt={props.title} />
-      </div>
-      <div className={classes.content}>
-        <h3>{props.title}</h3>
-        <address>{props.address}</address>
-        <p>{props.description}</p>
-      </div>
-      <div className={classes.actions}>
-        <button>To Favorite</button>
-      </div>
+      <Card>
+      
+        <div> 
+          <img src={props.image} alt={props.title} />
+        </div>
+
+        <div className={classes.content}>
+          <h3> {props.title}</h3>
+          <address> {props.address}</address>
+          <p> {props.description}</p>
+        </div>
+
+        <div className={classes.actions}>
+          <button>To Favorite</button>
+        </div>
+      </Card>
     </li>
   );
 }
