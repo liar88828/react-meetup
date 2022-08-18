6ed0a49386b2e2b9bfc13dcde7fb1940be3b1dab
liar780@gmail.com <liar780@gmail.com>
2022-08-18 20:06:15 +0700

    add function "usestate"

--- src/store/favorites-context.js
+++ src/store/favorites-context.js
@@ -1,15 +1,36 @@
- import { createContext } from "react";
+import { createContext, useState } from "react";
 
- const FavoritesContext=createContext({
-    favorites:[],
-    totalFavorites:0
- });
+const FavoritesContext = createContext({
+  favorites: [],
+  totalFavorites: 0,
+});
 
- functionFavoritesContextProvider(props){
+function FavoritesContextProvider(props) {
+  const [userFavorites, setUserFavorites] = useState([]);
 
-    const context={};
-return <FavoritesContext.Provider value={context}>
-    {props.children}
-</FavoritesContext.Provider>
+  function addFavoritesHandler(favoriteMeetup) {
+    setUserFavorites((prevUserFavorites) => {
+      return prevUserFavorites.concat(favoriteMeetup);
+    });
+  }
 
- }
+  function removeFavoriteHandler(meetupId) {
+    setUserFavorites((prevUserFavorites) => {
+      return prevUserFavorites.filter((meetup) => meetup.id !== meetupId);
+    });
+  }
+
+  function itemFavoriteHandler(meetupId) {
+    return userFavorites.some((meetup) => meetup.id === meetupId);
+  }
+
+  const context = {
+    favorites: userFavorites,
+    totalFavorites: userFavorites.length,
+  };
+  return (
+    <FavoritesContext.Provider value={context}>
+      {props.children}
+    </FavoritesContext.Provider>
+  );
+}
